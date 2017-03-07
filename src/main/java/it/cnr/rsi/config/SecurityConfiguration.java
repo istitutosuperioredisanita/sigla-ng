146da4924a9fb59f71614714eaaa342f6323a81b
Francesco Uliana <francesco.uliana@cnr.it>
2017-03-07 15:43:27 +0100

    ldap

--- src/main/java/it/cnr/rsi/config/SecurityConfiguration.java
+++ src/main/java/it/cnr/rsi/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package it.cnr.rsi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -18,17 +19,37 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-
         web.ignoring()
                 .antMatchers("/app/**/*.{js,html}");
     }
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                .disable();
+            .authorizeRequests()
+            .anyRequest().fullyAuthenticated()
+            .and()
+            .formLogin()
+            .and()
+            .csrf()
+            .disable();
     }
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userSearchBase("o=cnr, c=it")
+                .userSearchFilter("uid={0}")
+                .groupSearchBase(null)
+                .contextSource()
+                .url("ldap://virtest1.si.cnr.it:389")
+                .managerDn("cn=mastercnrapp2,ou=account,o=cnr,c=it")
+                .managerPassword("pippa");
+
+    }
+
+
 
 }
