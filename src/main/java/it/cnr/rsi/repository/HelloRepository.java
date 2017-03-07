fd2d556052b7821008efbb4c2fd77fea1ac51b43
Francesco Uliana <francesco.uliana@cnr.it>
2017-03-07 15:30:39 +0100

    dto

---
+++ src/main/java/it/cnr/rsi/repository/HelloRepository.java
@@ -0,0 +1,7 @@
+package it.cnr.rsi.repository;
+
+/**
+ * Created by francesco on 07/03/17.
+ */
+public class HelloRepository {
+}
---
+++ src/main/java/it/cnr/rsi/web/HelloResource.java
@@ -0,0 +1,7 @@
+package it.cnr.rsi.web;
+
+/**
+ * Created by francesco on 07/03/17.
+ */
+public class HelloResource {
+}
