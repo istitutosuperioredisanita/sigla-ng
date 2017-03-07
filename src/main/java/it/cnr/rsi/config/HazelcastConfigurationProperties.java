9a4fb6f3453419cb1a957737e0c52c6dcbbc597c
Francesco Uliana <francesco.uliana@cnr.it>
2017-03-07 16:15:55 +0100

    hazelcast

---
+++ src/main/java/it/cnr/rsi/config/HazelcastConfigurationProperties.java
@@ -0,0 +1,50 @@
+package it.cnr.rsi.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by francesco on 07/03/17.
+ */
+
+@ConfigurationProperties("cnr.hazelcast")
+public class HazelcastConfigurationProperties {
+    private int port;
+    private String instanceName;
+    private int timeToLiveSeconds;
+    private int backupCount;
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getInstanceName() {
+        return instanceName;
+    }
+
+    public void setInstanceName(String instanceName) {
+        this.instanceName = instanceName;
+    }
+
+    public int getTimeToLiveSeconds() {
+        return timeToLiveSeconds;
+    }
+
+    public void setTimeToLiveSeconds(int timeToLiveSeconds) {
+        this.timeToLiveSeconds = timeToLiveSeconds;
+    }
+
+    public int getBackupCount() {
+        return backupCount;
+    }
+
+    public void setBackupCount(int backupCount) {
+        this.backupCount = backupCount;
+    }
+
+
+
+}
