466313f556bd5d7eef6b846b8bfb0c6760e1e21b
Francesco Uliana <francesco.uliana@cnr.it>
2017-03-07 14:58:44 +0100

    init

---
+++ src/test/java/it/cnr/rsi/SiglaApplicationTests.java
@@ -0,0 +1,16 @@
+package it.cnr.rsi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SiglaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
