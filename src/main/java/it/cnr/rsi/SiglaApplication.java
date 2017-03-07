ca126aa4ca3f4c2b7e0d1cb71dab902f93b48302
Francesco Uliana <francesco.uliana@cnr.it>
2017-03-07 14:58:44 +0100

    init

---
+++ src/main/java/it/cnr/rsi/SiglaApplication.java
@@ -0,0 +1,12 @@
+package it.cnr.rsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiglaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiglaApplication.class, args);
+	}
+}
