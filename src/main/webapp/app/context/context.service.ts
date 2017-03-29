e612345412a3ddd390765c402a429b9a87cf3200
marco.spasiano <marco.spasiano@cnr.it>
2017-03-29 20:43:33 +0200

    Selezione cds e uo

--- src/main/webapp/app/context/context.service.ts
+++ src/main/webapp/app/context/context.service.ts
@@ -1,12 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, URLSearchParams } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-import { UserContext } from '../shared';
+import { UserContext, Principal } from '../shared';
 import { Pair } from './pair.model';
 
 @Injectable()
 export class ContextService  {
-    constructor(private http: Http) { }
+    esercizi: number[];
+    cdsPairs: Pair[];
+    uoPairs: Pair[];
+    cdrPairs: Pair[];
+    cdsModel: Pair;
+    uoModel: Pair;
+    cdrModel: Pair;
+
+    constructor(
+        private http: Http,
+        public principal: Principal
+    ) {
+        this.getEsercizi()
+            .subscribe(esercizi => this.esercizi = esercizi);
+        this.getCds(this.principal.getAccount().uo)
+            .subscribe(cds => {
+                let that = this;
+                this.cdsPairs = cds;
+                this.cdsModel = cds.filter(function(v) {
+                    return v.first === that.principal.getAccount().cds;
+                })[0];
+            });
+        this.getUo(this.principal.getAccount().cds)
+            .subscribe(uo => {
+                let that = this;
+                this.uoPairs = uo;
+                this.uoModel = uo.filter(function(v) {
+                    return v.first === that.principal.getAccount().uo;
+                })[0];
+            });
+     }
 
     getEsercizi(): Observable<number[]> {
         return this.http.get('/api/context/esercizio').map((res: Response) => res.json());
