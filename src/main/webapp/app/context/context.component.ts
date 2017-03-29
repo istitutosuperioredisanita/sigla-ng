c786ecd97017e19b9c1ac78c3a12d56dd0288282
marco.spasiano <marco.spasiano@cnr.it>
2017-03-29 20:43:33 +0200

    Selezione cds e uo

--- src/main/webapp/app/context/context.component.ts
+++ src/main/webapp/app/context/context.component.ts
@@ -11,20 +11,14 @@ import { Pair } from './pair.model';
 @Component({
     selector: 'jhi-context',
     templateUrl: './context.component.html',
-    providers: [ContextService, LocalStateStorageService, NgbDropdown]
+    providers: [LocalStateStorageService, NgbDropdown]
 })
+
 export class ContextComponent implements OnInit {
     @Input() isNavbar: boolean;
-    esercizi: number[];
-    cdsPairs: Pair[];
-    uoPairs: Pair[];
-    cdrPairs: Pair[];
-    cdsModel: Pair;
-    uoModel: Pair;
-    cdrModel: Pair;
 
     constructor(
-        private contextService: ContextService,
+        public contextService: ContextService,
         public principal: Principal,
         private localStateStorageService: LocalStateStorageService,
         private languageService: JhiLanguageService,
@@ -37,14 +31,14 @@ export class ContextComponent implements OnInit {
         text$
         .debounceTime(200)
         .map(term => term === '' ? []
-            : this.cdsPairs.filter(v => new RegExp(term, 'gi').test(v.first) || new RegExp(term, 'gi').test(v.second))
+            : this.contextService.cdsPairs.filter(v => new RegExp(term, 'gi').test(v.first) || new RegExp(term, 'gi').test(v.second))
         .slice(0, 10));
 
     searchuo = (text$: Observable<string>) =>
         text$
         .debounceTime(200)
         .map(term => term === '' ? []
-            : this.uoPairs.filter(v => new RegExp(term, 'gi').test(v.first) || new RegExp(term, 'gi').test(v.second))
+            : this.contextService.uoPairs.filter(v => new RegExp(term, 'gi').test(v.first) || new RegExp(term, 'gi').test(v.second))
         .slice(0, 10));
 
     formatter = (pair: Pair) => pair.first + ' - ' + pair.second;
@@ -54,9 +48,9 @@ export class ContextComponent implements OnInit {
         this.contextService
             .getUo(item ? item.first : '')
             .subscribe(uo => {
-                this.uoPairs = uo;
+                this.contextService.uoPairs = uo;
                 if (uo.length === 1) {
-                    this.uoModel = uo[0];
+                    this.contextService.uoModel = uo[0];
                 }
             });
     }
@@ -64,39 +58,17 @@ export class ContextComponent implements OnInit {
         this.contextService
             .getCds(item ? item.first : '')
             .subscribe(cds =>  {
-                this.cdsPairs = cds;
+                this.contextService.cdsPairs = cds;
                 if (cds.length === 1) {
-                    this.cdsModel = cds[0];
+                    this.contextService.cdsModel = cds[0];
                 }
             });
     }
 
     ngOnInit(): void {
         this.contextService
-            .getEsercizi()
-            .subscribe(esercizi => this.esercizi = esercizi);
-        this.contextService
             .saveUserContext(this.localStateStorageService.getUserContext())
             .subscribe(identity => this.principal.authenticate(identity));
-
-        this.contextService
-            .getCds(this.principal.getAccount().uo)
-            .subscribe(cds => {
-                let that = this;
-                this.cdsPairs = cds;
-                this.cdsModel = cds.filter(function(v) {
-                    return v.first === that.principal.getAccount().cds;
-                })[0];
-            });
-        this.contextService
-            .getUo(this.principal.getAccount().cds)
-            .subscribe(uo => {
-                let that = this;
-                this.uoPairs = uo;
-                this.uoModel = uo.filter(function(v) {
-                    return v.first === that.principal.getAccount().uo;
-                })[0];
-            });
     }
 
     setEsercizio(esercizio: number): void {
@@ -109,8 +81,8 @@ export class ContextComponent implements OnInit {
     saveContext(): void {
         let userContext = new UserContext(
                 this.principal.getAccount().esercizio,
-                this.cdsModel.first,
-                this.uoModel.first,
+                this.contextService.cdsModel.first,
+                this.contextService.uoModel.first,
                 this.principal.getAccount().cdr
             );
         this.contextService
