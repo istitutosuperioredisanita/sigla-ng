44a9ea7ec2a88467e5a9e9f7d4f9a72bf8383f40
marco.spasiano <marco.spasiano@cnr.it>
2017-04-03 18:34:53 +0200

    tree

--- src/main/webapp/app/workspace/index.ts
+++ src/main/webapp/app/workspace/index.ts
@@ -1,4 +1,5 @@
 export * from './workspace.route';
 export * from './workspace.component';
 export * from './workspace.service';
+export * from './tree.component';
 export * from './tree-leaf.component';
