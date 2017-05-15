f54dc107b285156a05aa22808845f07d3fdd23fc
marco.spasiano <marco.spasiano@cnr.it>
2017-05-15 17:07:31 +0200

    Gestione login e contesto

--- src/main/webapp/app/shared/login/login.service.ts
+++ src/main/webapp/app/shared/login/login.service.ts
@@ -4,6 +4,7 @@ import { JhiLanguageService } from 'ng-jhipster';
 import { Principal } from '../auth/principal.service';
 import { AuthServerProvider } from '../auth/auth-session.service';
 import { Router } from '@angular/router';
+import { LocalStateStorageService } from '../auth/local-storage.service';
 
 @Injectable()
 export class LoginService {
@@ -12,7 +13,8 @@ export class LoginService {
         private languageService: JhiLanguageService,
         private principal: Principal,
         private authServerProvider: AuthServerProvider,
-        private router: Router
+        private router: Router,
+        private localStateStorageService: LocalStateStorageService
     ) {}
 
     login (credentials, callback?) {
@@ -20,16 +22,16 @@ export class LoginService {
 
         return new Promise((resolve, reject) => {
             this.authServerProvider.login(credentials).subscribe(data => {
-                this.principal.identity(true).then(account => {
-                    this.authServerProvider.loginWildfly(credentials).subscribe(dataWildfly => {
+                this.authServerProvider.loginWildfly(credentials, this.localStateStorageService.getUserContext()).subscribe(dataWildfly => {
+                    this.principal.identity(true).then(account => {
+                            // After the login the language will be changed to
+                            // the language selected by the user during his registration
+                            if (account !== null) {
+                                this.languageService.changeLanguage(account.langKey);
+                            }
+                            resolve(data);
+                        });
                     });
-                    // After the login the language will be changed to
-                    // the language selected by the user during his registration
-                    if (account !== null) {
-                        this.languageService.changeLanguage(account.langKey);
-                    }
-                    resolve(data);
-                });
                 return cb();
             }, err => {
                 this.logout();
@@ -45,7 +47,9 @@ export class LoginService {
     }
 
     logout () {
-        this.authServerProvider.logout().subscribe();
+        this.authServerProvider.logout().subscribe(() => {
+            this.authServerProvider.logoutWildfly().subscribe();
+        });
         this.principal.authenticate(null);
     }
 }
