6127d1694190cabfcb63136018b26cdb99c92517
marco.spasiano <marco.spasiano@cnr.it>
2017-11-07 16:05:41 +0100

    angular 4

--- src/main/webapp/app/blocks/interceptor/http.provider.ts
+++ src/main/webapp/app/blocks/interceptor/http.provider.ts
@@ -5,19 +5,24 @@ import { Http, XHRBackend, RequestOptions } from '@angular/http';
 import { AuthExpiredInterceptor } from './auth-expired.interceptor';
 import { ErrorHandlerInterceptor } from './errorhandler.interceptor';
 import { NotificationInterceptor } from './notification.interceptor';
+import { StateStorageService } from '../../shared/auth/state-storage.service';
+import { Router } from '@angular/router';
 
 export function interceptableFactory(
     backend: XHRBackend,
     defaultOptions: RequestOptions,
     injector: Injector,
-    eventManager: JhiEventManager
+    eventManager: JhiEventManager,
+    stateStorageService: StateStorageService,
+    router: Router
 ) {
     return new JhiInterceptableHttp(
         backend,
         defaultOptions,
         [
+            new ErrorHandlerInterceptor(eventManager),            
+            new AuthExpiredInterceptor(injector, stateStorageService, router),
             // Other interceptors can be added here
-            new ErrorHandlerInterceptor(eventManager),
             new NotificationInterceptor(injector)
         ]
     );
@@ -31,7 +36,9 @@ export function customHttpProvider() {
             XHRBackend,
             RequestOptions,
             Injector,
-            JhiEventManager
+            JhiEventManager,
+            StateStorageService,
+            Router
         ]
     };
 }
