96ea1af7a4b60f6e0597ebd32792229ee31d0d72
marco.spasiano <marco.spasiano@cnr.it>
2017-11-19 12:08:35 +0100

    FIX messaggi

--- src/main/webapp/app/context/messaggio.model.ts
+++ src/main/webapp/app/context/messaggio.model.ts
@@ -3,6 +3,7 @@ export class Messaggio {
         public pgMessaggio: number,
         public checked: boolean,
         public cdUtente: string,
+        public soggetto: string,
         public corpo: string,
         public dsMessaggio: string,
         public dacr: Date
