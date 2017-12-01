81c1ce2fb85e6efbb915c27eefcb32f19f58f18d
marco.spasiano <marco.spasiano@cnr.it>
2017-12-01 08:43:41 +0100

    FIX form a ction form internet explorer 11

--- src/main/webapp/sigla-main.js
+++ src/main/webapp/sigla-main.js
@@ -1,3 +1,14 @@
+(function () {
+      if ( typeof window.CustomEvent === "function" ) return false;
+      function CustomEvent ( event, params ) {
+        params = params || { bubbles: false, cancelable: false, detail: undefined };
+        var evt = document.createEvent( 'CustomEvent' );
+        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+        return evt;
+       }    
+      CustomEvent.prototype = window.Event.prototype;
+      window.CustomEvent = CustomEvent;
+})();
 function submitForm(comando) {
     document.mainForm.comando.value = comando;
     if (document.mainForm.scrollx) {
