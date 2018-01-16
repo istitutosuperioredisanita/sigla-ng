fc0f11e2caa324071c61c8352464f4d8682b163b
marco.spasiano <marco.spasiano@cnr.it>
2018-01-16 11:35:33 +0100

    Aggiunta gestione utenza template

--- src/test/java/it/cnr/rsi/service/UtentiMultipliServiceTest.java
+++ src/test/java/it/cnr/rsi/service/UtentiMultipliServiceTest.java
@@ -1,7 +1,5 @@
 package it.cnr.rsi.service;
 
-import it.cnr.rsi.domain.Preferiti;
-import it.cnr.rsi.domain.Utente;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -9,9 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.List;
-import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
