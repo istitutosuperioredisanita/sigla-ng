7f0bf5af1bed2210466665628f98b6e3d0360672
marco.spasiano <marco.spasiano@cnr.it>
2018-02-01 18:30:54 +0100

    FIX compatibilità postgres

--- src/main/java/it/cnr/rsi/repository/UtenteRepository.java
+++ src/main/java/it/cnr/rsi/repository/UtenteRepository.java
@@ -18,7 +18,7 @@ public interface UtenteRepository extends JpaRepository<Utente, String> {
 	@Query("select a from Utente a where cdUtente = :cdUtente AND flAutenticazioneLdap = :flAutenticazioneLdap")
 	Utente findUserWithAuthenticationLDAP(@Param("cdUtente")String cdUtente, @Param("flAutenticazioneLdap")String flAutenticazioneLdap);
 
-	@Query("select a from Utente a where (cdUtenteUid = :cdUtenteUid OR cdUtente = :cdUtenteUid) AND (dtFineValidita >= SYSDATE OR dtFineValidita is null) AND flAutenticazioneLdap = 'Y' ")
+	@Query("select a from Utente a where (cdUtenteUid = :cdUtenteUid OR cdUtente = :cdUtenteUid) AND (dtFineValidita >= CURRENT_TIMESTAMP OR dtFineValidita is null) AND flAutenticazioneLdap = 'Y' ")
     List<Utente> findUsersForUid(@Param("cdUtenteUid")String cdUtenteUid);
 
 }
