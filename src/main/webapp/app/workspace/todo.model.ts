75b9d3518d3042386184a8c3741fe8f58c436d71
marco.spasiano <marco.spasiano@cnr.it>
2018-04-12 12:38:05 +0200

    Refactor todo

--- src/main/webapp/app/workspace/todo.model.ts
+++ src/main/webapp/app/workspace/todo.model.ts
@@ -3,6 +3,7 @@ export class TODO {
         public cdNodo: string,
         public faClass: string,
         public headerLabel: string,
+        public firstLabel: string,
         public label: string
     ) { }
 }
