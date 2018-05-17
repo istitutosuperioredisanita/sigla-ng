e629106a0e11e9cc4e03d6ffd7c730992233808d
marco.spasiano <marco.spasiano@cnr.it>
2018-05-17 22:10:18 +0200

    add ajp

--- src/main/java/it/cnr/rsi/SiglaApplication.java
+++ src/main/java/it/cnr/rsi/SiglaApplication.java
@@ -1,7 +1,12 @@
 package it.cnr.rsi;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SiglaApplication {
@@ -9,4 +14,35 @@ public class SiglaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SiglaApplication.class, args);
 	}
+    @Value("${tomcatAjp.protocol}")
+    String ajpProtocol;
+
+    @Value("${tomcatAjp.port}")
+    String ajpPort;
+
+    @Value("${tomcatAjp.enabled}")
+    String ajpEnabled;
+
+    @Value("${tomcatAjp.scheme}")
+    String ajpScheme;
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+
+        Integer ajpPortInt = Integer.parseInt(ajpPort);
+        Boolean ajpEnabledBool = Boolean.valueOf(ajpEnabled);
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+        if (ajpEnabledBool)
+        {
+            Connector ajpConnector = new Connector(ajpProtocol);
+            ajpConnector.setPort(ajpPortInt);
+            ajpConnector.setSecure(false);
+            ajpConnector.setAllowTrace(false);
+            ajpConnector.setScheme(ajpScheme);
+            tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        }
+
+        return tomcat;
+    }
+
 }
