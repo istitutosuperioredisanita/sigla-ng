3d00a501695a61c9fe0d624a5ffd618263254d23
marco.spasiano <marco.spasiano@cnr.it>
2018-05-18 12:16:41 +0200

    FIX context root

--- src/main/java/it/cnr/rsi/SiglaApplication.java
+++ src/main/java/it/cnr/rsi/SiglaApplication.java
@@ -14,35 +14,4 @@ public class SiglaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SiglaApplication.class, args);
 	}
-    @Value("${tomcatAjp.protocol}")
-    String ajpProtocol;
-
-    @Value("${tomcatAjp.port}")
-    String ajpPort;
-
-    @Value("${tomcatAjp.enabled}")
-    String ajpEnabled;
-
-    @Value("${tomcatAjp.scheme}")
-    String ajpScheme;
-    @Bean
-    public EmbeddedServletContainerFactory servletContainer() {
-
-        Integer ajpPortInt = Integer.parseInt(ajpPort);
-        Boolean ajpEnabledBool = Boolean.valueOf(ajpEnabled);
-
-        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-        if (ajpEnabledBool)
-        {
-            Connector ajpConnector = new Connector(ajpProtocol);
-            ajpConnector.setPort(ajpPortInt);
-            ajpConnector.setSecure(false);
-            ajpConnector.setAllowTrace(false);
-            ajpConnector.setScheme(ajpScheme);
-            tomcat.addAdditionalTomcatConnectors(ajpConnector);
-        }
-
-        return tomcat;
-    }
-
 }
