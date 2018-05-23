422471ba8dd9dbbc6d1ae36d94acecf89185edb5
marco.spasiano <marco.spasiano@cnr.it>
2018-05-23 15:20:57 +0200

    FIX accessi da uo afferente

--- src/main/java/it/cnr/rsi/repository/UnitaOrganizzativaRepository.java
+++ src/main/java/it/cnr/rsi/repository/UnitaOrganizzativaRepository.java
@@ -14,14 +14,22 @@ import java.util.stream.Stream;
 
 @Repository
 public interface UnitaOrganizzativaRepository extends JpaRepository<UnitaOrganizzativa, String> {
-    @Query("select a from UnitaOrganizzativa a join a.utenteUnitaRuolos ruolos " +
+    @Query("select a from UnitaOrganizzativa a, UnitaOrganizzativa b " +
+        " join b.utenteUnitaRuolos ruolos " +
+        " join b.cdrs cdr1 " +
+        " join Cdr cdr2 on cdr2.cdr = cdr1 or cdr2.cdCentroResponsabilita = cdr1.cdCentroResponsabilita " +
         "where ruolos.id.cdUtente = :userId" +
+        " and a = cdr2.unitaOrganizzativa" +
         " and (a.unitaOrganizzativaPadre.cdUnitaOrganizzativa = :cds OR :cds is null)" +
         " and a.esercizioInizio <= :esercizio and a.esercizioFine >= :esercizio")
     Stream<UnitaOrganizzativa> findUnitaOrganizzativeAbilitateByRuolo(@Param("userId") String userId, @Param("esercizio") Integer esercizio, @Param("cds") String cds);
 
-    @Query("select a from UnitaOrganizzativa a join a.utenteUnitaAccessos accessos " +
+    @Query("select a from UnitaOrganizzativa a, UnitaOrganizzativa b  " +
+        " join b.utenteUnitaAccessos accessos " +
+        " join b.cdrs cdr1 " +
+        " join Cdr cdr2 on cdr2.cdr = cdr1 or cdr2.cdCentroResponsabilita = cdr1.cdCentroResponsabilita " +
         "where accessos.id.cdUtente = :userId" +
+        " and a = cdr2.unitaOrganizzativa" +
         " and (a.unitaOrganizzativaPadre.cdUnitaOrganizzativa = :cds OR :cds is null)" +
         " and a.esercizioInizio <= :esercizio and a.esercizioFine >= :esercizio")
     Stream<UnitaOrganizzativa> findUnitaOrganizzativeAbilitateByAccesso(@Param("userId") String userId, @Param("esercizio") Integer esercizio, @Param("cds") String cds);
@@ -37,16 +45,31 @@ public interface UnitaOrganizzativaRepository extends JpaRepository<UnitaOrganiz
         " and a.esercizioInizio <= :esercizio and a.esercizioFine >= :esercizio and a.flCds = 'N'")
     Stream<UnitaOrganizzativa> findCdsValido(@Param("esercizio") Integer esercizio, @Param("cdUnitaOrganizzativa") String cdUnitaOrganizzativa);
 
-    @Query("select a.unitaOrganizzativaPadre from UnitaOrganizzativa a join a.utenteUnitaRuolos ruolos " +
+    @Query("select a.unitaOrganizzativaPadre from UnitaOrganizzativa a, UnitaOrganizzativa b " +
+        " join b.utenteUnitaRuolos ruolos " +
+        " join b.cdrs cdr1 " +
+        " join Cdr cdr2 on cdr2.cdr = cdr1 or cdr2.cdCentroResponsabilita = cdr1.cdCentroResponsabilita " +
         "where ruolos.id.cdUtente = :userId" +
+        " and a = cdr2.unitaOrganizzativa" +
         " and (a.cdUnitaOrganizzativa = :cdUnitaOrganizzativa OR :cdUnitaOrganizzativa is null)" +
         " and a.esercizioInizio <= :esercizio and a.esercizioFine >= :esercizio")
     Stream<UnitaOrganizzativa> findCdsAbilitatiByRuolo(@Param("userId") String userId, @Param("esercizio") Integer esercizio, @Param("cdUnitaOrganizzativa") String cdUnitaOrganizzativa);
 
-    @Query("select a.unitaOrganizzativaPadre from UnitaOrganizzativa a join a.utenteUnitaAccessos accessos " +
+    @Query("select a.unitaOrganizzativaPadre from UnitaOrganizzativa a, UnitaOrganizzativa b " +
+        " join b.utenteUnitaAccessos accessos " +
+        " join b.cdrs cdr1 " +
+        " join Cdr cdr2 on cdr2.cdr = cdr1 or cdr2.cdCentroResponsabilita = cdr1.cdCentroResponsabilita " +
         "where accessos.id.cdUtente = :userId" +
+        " and a = cdr2.unitaOrganizzativa" +
         " and (a.cdUnitaOrganizzativa = :cdUnitaOrganizzativa OR :cdUnitaOrganizzativa is null)" +
         " and a.esercizioInizio <= :esercizio and a.esercizioFine >= :esercizio")
     Stream<UnitaOrganizzativa> findCdsAbilitatiByAccesso(@Param("userId") String userId, @Param("esercizio") Integer esercizio, @Param("cdUnitaOrganizzativa") String cdUnitaOrganizzativa);
+
+    @Query("select distinct a.cdUnitaOrganizzativa from UnitaOrganizzativa a, Cdr cdr1 " +
+        " join Cdr cdr2 on cdr1.cdr = cdr2 " +
+        "where cdr1.unitaOrganizzativa.cdUnitaOrganizzativa = :cdUnitaOrganizzativa" +
+        " and a = cdr2.unitaOrganizzativa" +
+        " and a.esercizioInizio <= :esercizio and a.esercizioFine >= :esercizio")
+    Stream<String> findCodiceUoParents(@Param("esercizio") Integer esercizio, @Param("cdUnitaOrganizzativa") String cdUnitaOrganizzativa);
 
 }
