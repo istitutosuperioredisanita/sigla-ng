e3402cad1a109ffccc883cb965cd2eb24e315461
marco.spasiano <marco.spasiano@cnr.it>
2019-03-21 12:48:16 +0100

    FIX firefox tooltip not show for button and change esercizio from navbar

--- src/main/webapp/app/shared/auth/local-storage.service.ts
+++ src/main/webapp/app/shared/auth/local-storage.service.ts
@@ -4,27 +4,28 @@ import { UserContext } from '../../shared';
 
 @Injectable()
 export class LocalStateStorageService {
+    private USERCONTEXT = 'usercontext-';
     constructor(
         private $localStorage: LocalStorageService
     ) {}
 
     getUserContext(username: string): UserContext {
-        return this.$localStorage.retrieve('usercontext-' + username) ||
+        return this.$localStorage.retrieve(this.USERCONTEXT + username) ||
             new UserContext(new Date().getFullYear(), '', '', '');
     }
 
     resetUserContext(username: string) {
-        this.$localStorage.clear('usercontext-' + username);
+        this.$localStorage.clear(this.USERCONTEXT + username);
     }
 
     storeEsercizio(username: string, esercizio: number) {
         const userContext = this.getUserContext(username);
         userContext.esercizio = esercizio;
-        this.$localStorage.store('usercontext', userContext);
+        this.$localStorage.store(this.USERCONTEXT + username, userContext);
     }
 
     storeUserContext(username: string, usercontext: UserContext) {
-        this.$localStorage.store('usercontext-' + username, usercontext);
+        this.$localStorage.store(this.USERCONTEXT + username, usercontext);
     }
 
 }
