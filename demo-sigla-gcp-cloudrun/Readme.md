4a4c098a05d760328ea22cf20b3f6fb4b9056829
Villari Giuseppe <08301058@telecomitalia.it>
2022-03-08 15:25:50 +0100

    tidy up cloudrun demo

--- demo-sigla-gcp-cloudrun/Readme.md
+++ demo-sigla-gcp-cloudrun/Readme.md
@@ -26,3 +26,8 @@ E infine lanciare il comando di creazione
 
 ## Provalo su Google Cloud
 [![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://ssh.cloud.google.com/cloudshell/editor?cloudshell_git_repo=https://github.com/consiglionazionaledellericerche/sigla-ng.git&cloudshell_workspace=./demo-sigla-gcp-cloudrun&cloudshell_print=guide.txt&shellonly=true)
+
+# Update della versione di Sigla
+È posibile aggiornare la versione di Sigla, contestualmente all'aggiornamento delle immagini Docker ufficiali ([sigla-thorntail](https://hub.docker.com/r/consiglionazionalericerche/sigla-main/tags), [sigla-ng](https://hub.docker.com/r/consiglionazionalericerche/sigla-ng/tags)), tramite lo script update-service.sh.
+
+Lo script scarica le nuove immagini dal Docker Hub e le aggiorna sull'artifact registry del progetto, successivamente rilancia i file di configurazione .yaml in modo da aggiornare i container sui servizi Cloud Run.
--- demo-sigla-gcp-cloudrun/start-demo.sh
+++ demo-sigla-gcp-cloudrun/start-demo.sh
@@ -5,13 +5,39 @@ terraform init
 terraform apply -var="project_id=$project_id"
 cd ..
 
-chmod +x init-db.sh
-./init-db.sh
-chmod +x push-docker-images.sh
+#read password from prompt
+while true; do
+  read -s -p "Password: " password
+  echo
+  read -s -p "Password (confirm): " password2
+  echo
+  [ "$password" = "$password2" ] && break
+  echo "Please try again"
+done
+
+#Create a secret in GCP secret manager
+gcloud secrets create db-password
+echo -n $password | gcloud secrets versions add db-password --data-file=-
+
+#init database
+gcloud sql users create sigla --instance=pdb-team-digi-sigla-001 --password=$password
+gcloud sql databases create sigladb --instance=pdb-team-digi-sigla-001
+
+#Docker images
 ./push-docker-images.sh
 
+#give cloudrun accounts permissions reading secrets
+mapfile -t service_accounts < <(gcloud iam service-accounts list | grep EMAIL: | grep -E -o "\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b")
+
+for sa in "${service_accounts[@]}"
+do
+    gcloud projects add-iam-policy-binding $project_id --member="serviceAccount:$sa"  --role="roles/secretmanager.secretAccessor"
+done
+
+#get database private ip address
 export db_private_ip=$(gcloud sql instances describe pdb-team-digi-sigla-001 | grep '\-\sipAddress:' | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
 
+#deploy sigla service
 envsubst <  sigla-thorntail.yaml > sigla-thorntail-sub.yaml
 
 gcloud run services replace sigla-thorntail-sub.yaml
