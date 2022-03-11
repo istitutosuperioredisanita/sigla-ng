5b34b20436648d441efaeb171423ed16e80e99d7
Villari Giuseppe <08301058@telecomitalia.it>
2022-03-11 10:48:19 +0100

    Fix thorntail deploy on cloud run demo

--- demo-sigla-gcp-cloudrun/start-demo.sh
+++ demo-sigla-gcp-cloudrun/start-demo.sh
@@ -37,7 +37,17 @@ done
 #get database private ip address
 export db_private_ip=$(gcloud sql instances describe pdb-team-digi-sigla-001 | grep '\-\sipAddress:' | grep -E -o '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)')
 
-#deploy sigla service
+#DEPLOY SIGLA SERVICE
+#run sigla-ng to obtain his cloud run url
+envsubst <  sigla-ng.yaml > sigla-ng-temp.yaml
+
+gcloud run services replace sigla-ng-temp.yaml
+
+gcloud run services add-iam-policy-binding sigla-ng --member="allUsers"  --role="roles/run.invoker" --region=europe-west4
+
+export sigla_ng_url=$(gcloud run services describe sigla-ng --platform managed --region europe-west4 --format 'value(status.url)' --project $project_id)
+
+#run sigla-thorntail with all correct parameter
 envsubst <  sigla-thorntail.yaml > sigla-thorntail-sub.yaml
 
 gcloud run services replace sigla-thorntail-sub.yaml
@@ -46,14 +56,7 @@ gcloud run services add-iam-policy-binding sigla-thorntail --member="allUsers"  
 
 export sigla_thorntail_url=$(gcloud run services describe sigla-thorntail --platform managed --region europe-west4 --format 'value(status.url)' --project $project_id)
 
+#update sigla-ng with sigla-thorntail cloud run url
 envsubst <  sigla-ng.yaml > sigla-ng-sub.yaml
 
 gcloud run services replace sigla-ng-sub.yaml
-
-gcloud run services add-iam-policy-binding sigla-ng --member="allUsers"  --role="roles/run.invoker" --region=europe-west4
-
-export sigla_ng_url=$(gcloud run services describe sigla-ng --platform managed --region europe-west4 --format 'value(status.url)' --project $project_id)
-
-envsubst <  sigla-thorntail-final.yaml > sigla-thorntail-final-sub.yaml
-
-gcloud run services replace sigla-thorntail-final-sub.yaml
