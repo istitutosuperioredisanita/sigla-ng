50946d6d9787cf7a6a98a9a97b5665573019d9ea
cirosalvio <cirosalvio@hotmail.com>
2022-08-01 15:05:31 +0200

    Footer description management for different institute

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profiles/profile.service'; // FIXME barrel doesnt work here
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+     instituteAcronym: string;
+
+     constructor(
+             private profileService: ProfileService,
+         ) {
+         }
+
+     ngOnInit() {
+         this.profileService.getProfileInfo().subscribe((profileInfo) => {
+             this.instituteAcronym = profileInfo.instituteAcronym;
+            });
+     }
+ }
