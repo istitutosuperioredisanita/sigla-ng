0d19370dbcdbfa8db1d91b174b87e8b61fa8c66d
marco.spasiano <marco.spasiano@cnr.it>
2022-11-07 09:36:47 +0100

    Refactor remove all java resource

---
+++ src/main/webapp/env.template.js
@@ -0,0 +1,18 @@
+(function(window) {
+    window.env = window.env || {};
+  
+    // Environment variables
+    window["env"]["baseUrl"] = "${BASE_URL}";
+    window["env"]["applicationContextUrl"] = "${BASE_URL}${APPLICATION_CONTEXT}";
+    window["env"]["apiUrl"] = "${BASE_URL}${APPLICATION_CONTEXT}${API_CONTEXT}";
+
+    window["env"]["urlChangePassword"] = "${URL_CHANGE_PASSWORD}";
+    window["env"]["instituteAcronym"] = "${INSTITUTE_ACRONYM}";
+    window["env"]["ssoAppsMenuDisplay"] = "${SSO_APPS_MENU_DISPLAY}";
+
+    window["env"]["oidc.enable"] = "${OIDC_ENABLE}";
+    window["env"]["oidc.authority"] = "${OIDC_AUTHORITY}";
+    window["env"]["oidc.redirectUrl"] = "${OIDC_REDIRECTURL}";
+    window["env"]["oidc.clientId"] = "${OIDC_CLIENTID}";
+    window["env"]["oidc.postLogoutRedirectUri"] = "${OIDC_POSTLOGOUTREDIRECTURL}";
+})(this);
---
+++ src/main/webapp/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: false,
+  baseUrl: window['env']['baseUrl'] || 'default',
+  applicationContextUrl: window['env']['applicationContextUrl'] || 'default',
+  apiUrl: window['env']['apiUrl'] || 'default',
+  debug: window['env']['debug'] || false,
+  urlChangePassword: window['env']['urlChangePassword'],
+  instituteAcronym: window['env']['instituteAcronym'],
+  ssoAppsMenuDisplay: window['env']['ssoAppsMenuDisplay'] || false,
+  oidc: {
+    enable: window['env']['oidc.enable'] || false,
+    authority: window["env"]["oidc.authority"],
+    redirectUrl: window["env"]["oidc.redirectUrl"],
+    clientId: window['env']['oidc.clientId'],
+    postLogoutRedirectUri: window['env']['oidc.postLogoutRedirectUri'] || window.location.origin
+  }
+};
